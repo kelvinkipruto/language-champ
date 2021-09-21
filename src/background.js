f65250871f40a8c780408e1d77761e55c21c0fec
Kipruto <kvinkipruto@gmail.com>
2021-09-21 13:53:10 +0000

    init

---
+++ src/background.js
@@ -0,0 +1,10 @@
+browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  console.log(request);
+  console.log(sender);
+  console.log(sendResponse);
+  console.log("Hello from the background");
+
+  browser.tabs.executeScript({
+    file: "content-script.js",
+  });
+});
---
+++ src/options/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h3>Options Page</h3>
+  </div>
+</template>
+
+<script>
+import HelloWorld from "@/components/HelloWorld.vue";
+
+export default {
+  name: "App",
+  components: { HelloWorld },
+};
+</script>
+
+<style>
+html {
+  width: 400px;
+  height: 400px;
+}
+</style>
